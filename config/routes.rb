e9957e15bb53ea735beb4987adb9fb0d3dbcc539
Ricardo Lima <phpricardo@gmail.com>
2022-06-07 23:14:18 -0300

    Versionamento da API e response da criação de aluno

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  resources :bills
-  resources :enrollments
-  resources :students
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :bills
+      resources :enrollments
+      resources :students
+    end
+  end
 end
