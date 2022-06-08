35020e56a648fd7fb27ba9632c7858c39e0c2473
Ricardo Lima <phpricardo@gmail.com>
2022-06-08 00:32:28 -0300

    Algumas validações no students

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,2 +1,17 @@
+require "cpf_cnpj"
 class Student < ApplicationRecord
+  validates :name, :cpf, :payment_method, presence: true
+  validates_uniqueness_of :cpf, message: "Já existe um aluno com este número de CPF"
+
+  before_validation :check_cpf, on: :create
+
+  private
+
+  def check_cpf
+    if !CPF.valid?(cpf)
+      errors.add(:erro, "O CPF não é valido.")
+      return false
+    end
+  end
+
 end
