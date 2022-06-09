0ee9a99e9bddf9990b352672f0824415b664893b
Ricardo Lima <phpricardo@gmail.com>
2022-06-09 00:17:56 -0300

    Roda pra listar students com pagina e quantidade

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :bills
       resources :enrollments
+
       resources :students
+      get "students/:page/:count", to: "students#index"
     end
   end
 end
