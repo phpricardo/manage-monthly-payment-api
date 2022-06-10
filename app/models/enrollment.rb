b0055d783d2c4b59445ffdddb6b36ea6d9d9207b
Ricardo Lima <phpricardo@gmail.com>
2022-06-09 23:11:51 -0300

    callback para salvar bills

--- app/models/enrollment.rb
+++ app/models/enrollment.rb
@@ -3,4 +3,18 @@ class Enrollment < ApplicationRecord
   has_many :bills
 
   accepts_nested_attributes_for :bills
+
+  after_save :save_bills
+
+  def save_bills
+    quantity_installments = self.installments
+    amount = self.amount
+
+    bill_amount = amount / quantity_installments
+
+    while quantity_installments >= 1
+      Bill.create!(amount: bill_amount, enrollment_id: self.id)
+      quantity_installments -= 1
+    end
+  end
 end
