fce49a9e760bf8b318e50bf4d395bc3c58f014bc
Ricardo Lima <phpricardo@gmail.com>
2022-06-12 19:21:24 -0300

    Nova rota para enroolments com pagina e quantidade.

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :bills
       resources :enrollments
-
       resources :students
+      
       get "students/:page/:count", to: "students#index"
+      get "enrollments/:page/:count", to: "enrollments#index"
     end
   end
 end
