826ec783ef07e36077db95a468ada9b6d193b536
Ricardo Lima <phpricardo@gmail.com>
2022-06-12 19:24:50 -0300

    customizado json para formatar bithdate e validaçoes

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,14 +2,26 @@ require "cpf_cnpj"
 class Student < ApplicationRecord
   validates :name, :cpf, :payment_method, presence: true
   validates_uniqueness_of :cpf, message: "Já existe um aluno com este número de CPF"
+  validate :validate_payment_method
 
   before_validation :check_cpf, on: :create
+
+  def as_json(options={})
+    obj = super(options)
+    obj[:birthdate] = (I18n.l(self.birthdate) unless self.birthdate.blank?)
+    obj
+  end
 
   private
 
   def check_cpf
-    if !CPF.valid?(cpf)
-      errors.add(:erro, "O CPF não é valido.")
+    errors.add(:erro, "O CPF não é valido." ) if !CPF.valid?(cpf)
+  end
+
+  def validate_payment_method
+    payment_method = self.payment_method
+    if payment_method != "Boleto" && payment_method != "Cartão de Crédito" 
+      errors.add(:erro, "Método de pagamento inválido.")
       return false
     end
   end
